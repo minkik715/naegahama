52bce76081fbe4ded8bbc016eb2506e6071a5654
rlaalsrl715 <minkik715@gmail.com>
2022-03-07 10:29:29 +0900

    modify kakaoLogin

--- src/main/java/com/hanghae/naegahama/kakaologin/KakaoUserInfo.java
+++ src/main/java/com/hanghae/naegahama/kakaologin/KakaoUserInfo.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 @Getter
 public class KakaoUserInfo {
+    private Long id;
     private String nickname;
     private String email;
 
-    public KakaoUserInfo(String nickname, String email) {
+    public KakaoUserInfo(Long id,String nickname, String email) {
+        this.id = id;
         this.nickname = nickname;
         this.email = email;
     }
