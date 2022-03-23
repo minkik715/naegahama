22299332ccddaef6d6ec2d111313eac8e94b89e3
rlaalsrl715 <79639282+rlaalsrl715@users.noreply.github.com>
2022-03-24 03:30:19 +0900

    Update health.sh

--- scripts/health.sh
+++ scripts/health.sh
@@ -14,7 +14,7 @@ for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
-
+  echo "Response = ${UP_COUNT}"
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
