25fccd50f452d9114d95bb370d2fd846dbc3a32a
rlaalsrl715 <79639282+rlaalsrl715@users.noreply.github.com>
2022-03-24 04:38:12 +0900

    Update switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -2,7 +2,7 @@
 
 # Crawl current connected port of WAS
 
-CURRENT_PORT=$(cat /etc/nginx/conf.d/service_url.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 echo "> Nginx currently proxies to ${CURRENT_PORT}."
@@ -21,7 +21,7 @@ fi
 # tee 는
   # 출력 내용을 파일로 만들어주는 커맨드입니다.
   # 새로 띄운 WAS의 포트를 nginx가 읽을 수 있도록 service_url.inc에 내용을 덮어씁니다.
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /etc/nginx/conf.d/service_url.inc
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /etc/nginx/conf.d/service-url.inc
 
 echo "> Now Nginx proxies to ${TARGET_PORT}."
 
